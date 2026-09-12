1d6c009765e18a975ce06167aa5f374bdc850243
unknown <dwikukuh43@gmail.com>
2026-09-12 16:34:50 +0700

    feat: tambah informasi lokasi,jam, produk dan promo UMKM

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ const promoButton = document.querySelector('#promobutton');
 
 promoButton.addEventListener('click', () => {
     promoButton.textContent = "Promo Kopi Kajen: Beli 2 Gratis 1 Untuk Cold Brew Aren !";
-    console.log("Promo Kopi Kajen siap dinikmati! Yuk dukung UMKM kopi lokal.");
+    console.log("Promo Kopi Kajen: Beli 2 Gratis 1 Untuk Cold Brew Aren.");
 });
