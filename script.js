6cf17f7210ca452ae018b694c6f89e79c2898a76
kukuhDwi <dwikukuh43@gmail.com>
2026-09-12 17:11:10 +0700

    feat: tambah informasi lokasi, mnu, promo UMKM

---
+++ script.js
@@ -0,0 +1,6 @@
+const promoButton = document.querySelector('#promobutton');
+
+promoButton.addEventListener('click', () => {
+    promoButton.textContent = "Promo Kopi Kajen: Beli 2 Gratis 1 Untuk Cold Brew Aren !";
+    console.log("Promo Kopi Kajen: Beli 2 Gratis 1 Untuk Cold Brew Aren.");
+});
